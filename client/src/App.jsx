e14cf68d817b2c3c6e9605676916914f2e3b1a1d
praymondal@gmail.com <praymondal@gmail.com>
2023-03-17 08:26:07 -0400

    application skeleton complete

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Home from "./components/Home";
+import { Nav } from "./components/navBar/Nav";
+
+const App = () => {
+  return (
+    <div>
+      {/* nav component */}
+      <Nav />
+      {/* home component */}
+      <Home />
+      {/* mcl intranet_form */}
+      {/* mcl intranet_details */}
+      {/* footer component */}
+    </div>
+  );
+};
+
+export default App;
---
+++ client/tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // purge:[],
+  darkMode: "media",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
