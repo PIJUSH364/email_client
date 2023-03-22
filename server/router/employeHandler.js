f8ac45d32af383a994bc40b8cc76c2e2e883e161
PIJUSH364 <praymondal@gmail.com>
2023-03-23 00:52:37 +0530

    backend done

--- server/router/employeHandler.js
+++ server/router/employeHandler.js
@@ -2,22 +2,41 @@ const express = require('express');
 const mongoose = require('mongoose');
 const empSchema = require('../schema/employeeSchema');
 const contentSchema = require('../schema/tableContentSchema');
+const connectDb = require('../config/dbConnection');
 
 
 const User = new mongoose.model('User', empSchema);
-const employeeContent = new mongoose.model('content', contentSchema);
+const tableContent = new mongoose.model('Content', contentSchema);
 
 const contentRouter = express.Router();
 const employeeRouter = express.Router();
 const employeeInfoRouter = express.Router();
 
-contentRouter.post("/emp_content", (req, res) => {
+contentRouter.post("/emp_content", async (req, res) => {
     try {
-        const data = req.body;
-        console.log(data);
+        const { DocumentNo, refNo, DocumentType, title, section, department, search_keyWord } = req.body;
+        // new user create
+        const content = new tableContent({ DocumentNo, refNo, DocumentType, title, section, department, search_keyWord });
+        // user save on mongodb
+        await content.save();
+        // finally response send
+        res.status(200).send({ message: 'upload successful!' });
+
     } catch (error) {
-        console.log(error);
+        res.status(200).send({ message: 'upload failed!' });
     }
+})
+
+contentRouter.get("/emp_content", async (req, res) => {
+
+    tableContent.find({}, { _id: 0 }).then((result) => {
+        res.json(result)
+
+    }).catch((err) => {
+        res.json("something went wrong")
+    });
+
+    // res.send(collection)
 })
 
 employeeRouter.post("/sing_up", async (req, res) => {
